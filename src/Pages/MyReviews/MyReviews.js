5108b4df09dd8fd0bfd284ce0496aee6455f96ae
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-11-09 17:09:58 +0600

    delete a review

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import { BiTrash, BiEdit } from "react-icons/bi";
 
 const MyReviews = () => {
     const { user } = useContext(AuthContext);
@@ -12,39 +13,76 @@ const MyReviews = () => {
             });
     }, [user]);
 
+    // delete a review
+    const handelDelete = id => {
+        const confirmDelete = window.confirm(
+            "Are you sure want to delete order?"
+        );
+        if (confirmDelete) {
+            fetch(`http://localhost:5000/allReview/${id}`, {
+                method: "DELETE",
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert("Deleted Successfully");
+                        const remaining = userReviews.filter(
+                            review => review._id !== id
+                        );
+                        setUserReviews(remaining);
+                    }
+                });
+        }
+    };
+
     console.log(userReviews);
     return (
         <div className="my-6 p-4">
-            <h3 className="font-bold">All Previous Review of this service:</h3>
-            {userReviews.map(review => (
-                <div key={review._id} className="p-2 mb-2 border rounded-xl">
-                    <div className="flex">
-                        <div>
-                            <div className="mask mask-squircle w-10 h-10 mr-2">
-                                <img
-                                    src={
-                                        review?.photoURL
-                                            ? review?.photoURL
-                                            : "https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/face-icon.png"
-                                    }
-                                    alt="Avatar Tailwind CSS Component"
-                                />
-                            </div>
-                        </div>
-                        <div className=" p-2 bg-gray-200 rounded-2xl">
-                            <div>
-                                <h2 className="font-bold">
-                                    {review?.displayName}{" "}
-                                    <spam>({review?.email})</spam>
-                                </h2>
-                            </div>
-                            <div>
-                                <p className="text-justify">{review.review}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
+            <h3 className="font-bold text-2xl text-center">My Reviews:</h3>
+
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th>Service Name</th>
+                            <th>My Review</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row --> */}
+                        {userReviews.map(review => (
+                            <tr key={review._id} className="hover">
+                                <td>{review.serviceId}</td>
+                                <td>
+                                    <p>
+                                        {review.review.length > 100
+                                            ? `${review.review.slice(
+                                                  0,
+                                                  100
+                                              )} . . .`
+                                            : review.review}
+                                    </p>
+                                </td>
+                                <td>
+                                    {/* Edit Btn */}
+                                    <button className="btn mr-2 p-3">
+                                        <BiEdit className="w-6 h-6"></BiEdit>
+                                    </button>
+                                    {/* Delete Btn */}
+                                    <button
+                                        onClick={() => handelDelete(review._id)}
+                                        className="btn p-3"
+                                    >
+                                        <BiTrash className="w-6 h-6"></BiTrash>
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
